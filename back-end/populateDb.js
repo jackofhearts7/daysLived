fac18c163589b7ae3c2fc5b78018c89f10d04c9d
Brandon Jackson <jacksonbrandon2002@yahoo.com>
2022-12-01 01:36:21 +0000

    more progress on populating db, correcting data

--- back-end/populateDb.js
+++ back-end/populateDb.js
@@ -6,10 +6,13 @@ const { parse } = require("csv-parse");
 fs.createReadStream("./people.csv")
   .pipe(parse({ delimiter: ",", from_line: 1, relax_quotes: true}))
   .on("data", function (row) {
-      let birthDate = new Date(row[1]);
-      let deathDate = new Date(row[2]);
-      const person = { name: row[0], birth: birthDate, death: deathDate, link: row[3]};
+      let birthDate = new Date(row[1].trim());
+      let deathDate = new Date(row[2].trim());
+      let d = (deathDate - birthDate) / (1000 * 60 * 60 * 24);
+      //const person = { name: row[0], birth: birthDate, death: deathDate, link: row[3]};
+      const person = { name: row[0], birth: birthDate, death: deathDate, daysLived: d, link: row[3].trim()};
       //console.log(row);
       console.log(person);
   })
+
   
