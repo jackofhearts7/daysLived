9622124baaae0212dcbdbd3c5f3cf6875cb29157
Brandon Jackson <jacksonbrandon2002@yahoo.com>
2022-12-03 20:08:17 +0000

    added api's

--- back-end/app.js
+++ back-end/app.js
@@ -1,24 +1,14 @@
-const express = require('express');
-const bodyParser = require("body-parser");
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.static('public'));
-
+console.log('Starting up...');
 const mongoose = require('mongoose');
 
-mongoose.conntect('mongodb://localhost:27017/people', {
+console.log('Connecting to the database...');
+mongoose.connect('mongodb://localhost:27017/daysLived', {
   useUnifiedTopology: true,
   useNewURLParser: true,
 });
-
+//  TODO - handle case where connection fails
+console.log("Successfully connected to the database.");
 
 /*
     Startup routine. Check
@@ -30,4 +20,85 @@ mongoose.conntect('mongodb://localhost:27017/people', {
     
 */
 
+const personSchema = new mongoose.Schema({
+  name: String,
+  birth: Date,
+  death: Date,
+  daysLived: Number,
+  link: String
+});
+//  To facilitate Express/Vue code in using 'id' instead of "_id"
+personSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+personSchema.set('toJSON', { virtuals: true });
 
+const personModel = new mongoose.model('persons', personSchema);
+
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.static('public'));
+
+
+app.get('/api/persons', async (req, res) => {
+  try {
+    let persons = await personModel.find();
+    res.send({persons: persons});
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/api/persons', async (req, res) => {
+  console.log('req.body.birth = ' + req.body.birth);
+  console.log('req.body.death = ' + req.body.death);
+  const birth = new Date(req.body.birth);
+  console.log('birth = ' + birth);
+  const death = new Date(req.body.death);
+  console.log('death = ' + death);
+  const daysLived = ((death - birth) / (1000 * 60 * 60 * 24));
+  console.log('daysLived = ' + daysLived);
+  const person = new personModel({
+    name: req.body.name,
+    birth: req.body.birth,
+    death: req.body.death,
+    daysLived: daysLived,
+    link: req.body.link
+  });
+  try {
+    await person.save();
+    res.send({person:person});
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);    
+  }
+});
+
+app.delete('/api/persons/:id', async (req, res) => {
+  try {
+    await personModel.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);    
+  }
+});
+
+app.put('/api/persons/:id', async (req, res) => {
+  
+});
+
+app.listen( 3000, () => console.log('Server listening on port 3000.'));
