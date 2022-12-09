47502d04176457b2b34846d018abf50980d2fd88
Brandon Jackson <jacksonbrandon2002@yahoo.com>
2022-12-09 01:10:13 +0000

    changed port from 3000 to 8080

--- back-end/app.js
+++ back-end/app.js
@@ -119,4 +119,4 @@ app.put('/api/persons/:id', async (req, res) => {
   }
 });
 
-app.listen( 3000, () => console.log('Server listening on port 3000.'));
+app.listen( 8080, () => console.log('Server listening on port 8080.'));
