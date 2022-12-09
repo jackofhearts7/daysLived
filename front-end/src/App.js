3430b81f9dc2af22026846525d4ac56fab16ad9f
Jeremy Jackson <jeremyljackson00@gmail.com>
2022-12-08 23:57:17 -0700

    j

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import Match from './Match.js';
 
 function App() {
   //const [persons, setPersons] = useState([]);
-  const [persons, setPersons] = useState([{id:"123", _id: "123", name: "j", birth: "2000-02-19", death: "2100-02-19", daysLived: "36500", link: "http://jlyonj.org"  }]);
+  const [persons, setPersons] = useState([{id:"123", _id: "123", name: "j", birth: "2000-02-19", death: "2100-02-19", daysLived: "36500", link: "test.com"  }]);
 
   const [error, setError] = useState("");
   const [userBDay, setUserBDay] = useState("");
@@ -59,7 +59,10 @@ return (
       <div key = {person.id} className="person">
         {person.name}, {person.daysLived}, {person.link}
       </div>
+      
     ))}
+    <a href="https://github.com/jackofhearts7/daysLived"> Github Repo</a>
+
 
   </div>
 
